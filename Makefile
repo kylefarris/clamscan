4d712bff79dbffc283ffb864873002a71280c07c
SaltwaterC <saltwaterc@gmail.com>
2015-08-14 01:14:32 +0100

    Tidying up the development environment. Made the test suite to be more portable from one dev to another. The Makefile handles the test dependencies via the all target. The test environment is automatically created by the test target. Added a clean target for nuking node_modules.

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
+.PHONY: all
 TESTS = tests/*.js
-test:
-	mocha --timeout 5000 --check-leaks --reporter spec $(TESTS)
- 
-.PHONY: test
+
+all:
+	@npm install
+
+test: all
+	@mkdir -p tests/infected
+	@mkdir -p tests/bad_scan_dir
+	@touch tests/clamscan-log
+	@./node_modules/.bin/mocha --timeout 5000 --check-leaks --reporter spec $(TESTS)
+
+clean:
+	rm -rf node_modules
--- tests/test_config.js
+++ tests/test_config.js
@@ -1,13 +1,40 @@
+var fs = require('fs');
+var p = require('path');
+
+// walk $PATH to find bin
+var which = function (bin) {
+	var dir, file;
+	var path = process.env.PATH.split(p.delimiter);
+	for (i in path) {
+		file = path[i] + p.sep + bin;
+		if (fs.existsSync(file)) {
+			return file;
+		}
+	}
+	return '';
+};
+
+// return either $CLAMD_PATH or something like /usr/local/etc/clamav/clamd.conf
+var findClamdConf = function () {
+	if (process.env.CLAMD_PATH) {
+		return process.env.CLAMD_PATH;
+	}
+	var clamdscan = which('clamdscan');
+	clamdscan = clamdscan.split(p.sep);
+	clamdscan.splice(-2, 2);
+	return clamdscan.join(p.sep) + p.sep + 'etc/clamav/clamd.conf';
+};
+
 module.exports = {
 	remove_infected: false, 						// don't change
 	quarantine_infected: __dirname + '/infected', 	// required for testing
 	scan_log: __dirname + '/clamscan-log',  		// required for testing
 	clamscan: {
-		path: '/usr/bin/clamscan',  				// required for testing (change for your system)
+		path: which('clamscan'),  	// required for testing
 	},
 	clamdscan: {
-		path: '/usr/bin/clamdscan',  				// required for testing (change for your system)
-		config_file: '/etc/clamd.d/daemon.conf'  	// required for testing (change for your system)
+		path: which('clamdscan'),  	// required for testing
+		config_file: findClamdConf() || '/etc/clamd.d/daemon.conf'  	// required for testing
 	},
 	debug_mode: false
 };
