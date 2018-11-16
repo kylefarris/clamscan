3ff339fd6a2861f17c783d02c1875c67290205ac
Kyle Farris <kyle.farris@infotechfl.com>
2018-11-16 18:27:33 -0500

    More conversionto ES6.

--- ClamAVChannel.js
+++ ClamAVChannel.js
@@ -3,38 +3,38 @@
  * https://github.com/yongtang/clamav.js
  */
 
-var util = require('util');
-var Transform = require('stream').Transform;
+const util = require('util');
+const Transform = require('stream').Transform;
 util.inherits(ClamAVChannel, Transform);
 
-function ClamAVChannel(options) {
-    Transform.call(this, options);
-    this._inBody = false;
-}
-
-ClamAVChannel.prototype._transform = function(chunk, encoding, callback) {
-    if (!this._inBody) {
-        this.push("nINSTREAM\n");
-        this._inBody = true;
+class ClamAVChannel {
+    constructor(options) {
+        Transform.call(this, options);
+        this._inBody = false;
     }
 
-    var size = new Buffer(4);
-    size.writeInt32BE(chunk.length, 0);
-    this.push(size);
-    this.push(chunk);
+    _transform(chunk, encoding, callback) {
+        if (!this._inBody) {
+            this.push("nINSTREAM\n");
+            this._inBody = true;
+        }
 
-    callback();
-};
+        const size = new Buffer(4);
+        size.writeInt32BE(chunk.length, 0);
+        this.push(size);
+        this.push(chunk);
 
-ClamAVChannel.prototype._flush = function (callback) {
-    var size = new Buffer(4);
-    size = new Buffer(4);
-    size.writeInt32BE(0, 0);
-    this.push(size);
+        callback();
+    }
 
-    callback();
-};
+    _flush(callback) {
+        const size = new Buffer(4);
+        size = new Buffer(4);
+        size.writeInt32BE(0, 0);
+        this.push(size);
 
-module.exports = function(options) {
-    return new ClamAVChannel(options);
-};
+        callback();
+    }
+}
+
+module.exports = options => new ClamAVChannel(options);
