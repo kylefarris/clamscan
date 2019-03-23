e5fea1691f6027c0594fa59276cc78a033f89374
Kyle Farris <kyle.farris@infotechfl.com>
2019-03-23 01:17:45 -0400

    Added some examples.

---
+++ examples/basic_async_await.js
@@ -0,0 +1,60 @@
+const request = require('request');
+const fs = require('fs');
+const fake_virus_url = 'https://secure.eicar.org/eicar_com.txt';
+const normal_file_url = 'https://raw.githubusercontent.com/kylefarris/clamscan/sockets/README.md';
+const temp_dir = __dirname;
+const scan_file = `${temp_dir}/tmp_file.txt`;
+//const test_file = normal_file_url;
+const test_file = fake_virus_url;
+
+// Initialize the clamscan module
+const NodeClam = require('../index.js'); // Offically: require('clamscan');
+
+async function test() {
+    const clamscan = await new NodeClam().init({
+        remove_infected: true,
+        debug_mode: false,
+        scan_recursively: false,
+        clamdscan: {
+            path: '/usr/bin/clamdscan',
+            config_file: '/etc/clamd.d/daemon.conf'
+        },
+        preference: 'clamdscan'
+    });
+
+    // Request a test file from the internet...
+    request(test_file, async (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            // Write the file to the filesystem
+            fs.writeFileSync(scan_file, body);
+
+            // Scan the file
+            try {
+                const {file, is_infected, viruses} = await clamscan.is_infected(scan_file);
+
+                // If `is_infected` is TRUE, file is a virus!
+                if (is_infected === true) {
+                    console.log(`You've downloaded a virus (${viruses.join('')})! Don't worry, it's only a test one and is not malicious...`);
+                } else if (is_infected === null) {
+                    console.log("Something didn't work right...");
+                } else if (is_infected === false) {
+                    console.log(`The file (${file}) you downloaded was just fine... Carry on...`);
+                }
+
+                // Remove the file (for good measure)
+                if (fs.existsSync(scan_file)) fs.unlinkSync(scan_file);
+                process.exit(0);
+            } catch (err) {
+                console.error("ERROR: " + err);
+                console.trace(err.stack);
+                process.exit(1);
+            }
+        } else {
+            console.log("Could not download test virus file!");
+            console.error(error);
+            process.exit(1);
+        }
+    });
+}
+
+test();
---
+++ examples/stream.js
@@ -0,0 +1,43 @@
+const {PassThrough} = require('stream');
+const request = require('request');
+
+const fake_virus_url = 'https://secure.eicar.org/eicar_com.txt';
+const normal_file_url = 'https://raw.githubusercontent.com/kylefarris/clamscan/sockets/README.md';
+const test_url = normal_file_url;
+// const test_url = fake_virus_url;
+
+// Initialize the clamscan module
+const NodeClam = require('../index.js'); // Offically: require('clamscan');
+
+async function test() {
+    const clamscan = await new NodeClam().init({
+        clamdscan: {
+            socket: '/var/run/clamd.scan/clamd.sock',
+        },
+    });
+
+    const passthrough = new PassThrough();
+    const source = request.get(test_url);
+
+    // Fetch fake Eicar virus file and pipe it through to our scan screeam
+    source.pipe(passthrough);
+
+    try {
+        const {is_infected, viruses} = await clamscan.scan_stream(passthrough)
+
+        // If `is_infected` is TRUE, file is a virus!
+        if (is_infected === true) {
+            console.log(`You've downloaded a virus (${viruses.join('')})! Don't worry, it's only a test one and is not malicious...`);
+        } else if (is_infected === null) {
+            console.log("Something didn't work right...");
+        } else if (is_infected === false) {
+            console.log(`The file (${test_url}) you downloaded was just fine... Carry on...`);
+        }
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+test();
