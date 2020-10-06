37ee570657017c57f4934016e37a3644f8319f65
Alejandro Romero Herrera <alromh87@gmail.com>
2020-10-06 23:26:29 +0300

    Fix Command Injection after previous fix

--- index.js
+++ index.js
@@ -490,14 +490,13 @@ class NodeClam {
         }
 
         const version_cmds = {
-            clamdscan: `${path} --version`,
-            clamscan: `${path} --version`,
+            clamdscan: `--version`,
+            clamscan:  `--version`,
         };
 
         try {
             await fs_access(path, fs.constants.R_OK);
-            const version_cmds_exec = version_cmds[scanner].split(' ');
-            const {stdout} = await cp_execfile(version_cmds_exec[0], [version_cmds_exec[1]]);
+            const {stdout} = await cp_execfile(path, [version_cmds[scanner]]);
             if (stdout.toString().match(/ClamAV/) === null) {
                 if (this.settings.debug_mode) console.log(`${this.debug_label}: Could not verify the ${scanner} binary.`);
                 return false;
