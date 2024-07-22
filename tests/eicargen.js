b0bb93019c291e561225e9673424bbf8e57cc9d8
Kyle Farris <kylefarris@gmail.com>
2024-07-22 13:49:12 -0400

    Added consecutive spaces to bad file string for testing.

--- tests/eicargen.js
+++ tests/eicargen.js
@@ -14,7 +14,7 @@ const goodScanDir = `${__dirname}/good_scan_dir`;
 const badScanDir = `${__dirname}/bad_scan_dir`;
 const mixedScanDir = `${__dirname}/mixed_scan_dir`
 const badScanFile = `${badScanDir}/bad_file_1.txt`;
-const spacedVirusFile = `${badScanDir}/bad file 1.txt`;
+const spacedVirusFile = `${badScanDir}/bad  file  1.txt`;
 
 // prettier-ignore
 const eicarByteArray = [
